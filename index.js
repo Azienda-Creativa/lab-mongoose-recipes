1a7e485df5c5193d4f97bd6b56440f6a0cda06e0
Azienda-Creativa <116496155+Azienda-Creativa@users.noreply.github.com>
2023-02-12 18:32:38 +0100

    iteration 4 and 5 not updating db dunno why

--- index.js
+++ index.js
@@ -3,49 +3,76 @@ const mongoose = require("mongoose")
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require("./models/Recipe.model")
 // Import of the data from './data.json'
-const data = require("./data")
+const data = require("./data.json")
 mongoose.set("strictQuery", false)
 
-const MONGODB_URI = "mongodb://localhost:27017/recipe-app"
+const MONGODB_URI = "mongodb://127.0.0.1:27017/recipe-app"
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
+
   .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`)
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
 
+  // Iteration 2
   .then(() => {
     // Run your code here, after you have insured that the connection was made
-    Recipe.create({
-      title: "test",
-      level: "Amateur Chef",
-      ingredients: [
-        "1/2 cup rice vinegar",
-        "5 tablespoons honey",
-        "1/3 cup soy sauce (such as Silver Swan®)",
-        "1/4 cup Asian (toasted) sesame oil",
-        "3 tablespoons Asian chili garlic sauce",
-        "3 tablespoons minced garlic",
-        "salt to taste",
-        "8 skinless, boneless chicken thighs",
-      ],
-      cuisine: "Asian",
-      dishType: "main_course",
-      image:
-        "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
-      duration: 40,
-      creator: "Chef LePapu",
-    }).then((newRecipe) => {
-      console.log(newRecipe.title)
-    })
-  })
-  .catch((error) => {
-    console.error("Error connecting to the database", error)
-  })
+    // Recipe.create({
+    //   title: "Chicken Garlic",
+    //   level: "Amateur Chef",
+    //   ingredients: [
+    //     "1/2 cup rice vinegar",
+    //     "5 tablespoons honey",
+    //     "1/3 cup soy sauce (such as Silver Swan®)",
+    //     "1/4 cup Asian (toasted) sesame oil",
+    //     "3 tablespoons Asian chili garlic sauce",
+    //     "3 tablespoons minced garlic",
+    //     "salt to taste",
+    //     "8 skinless, boneless chicken thighs",
+    //   ],
+    //   cuisine: "Asian",
+    //   dishType: "main_course",
+    //   image:
+    //     "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+    //   duration: 40,
+    //   creator: "Chef LePapu",
+    // })
 
-  .then(() => {
+    //   .then((newRecipe) => {
+    //     console.log(newRecipe.title)
+    //   })
+    //   .catch((error) => {
+    //     console.error("Error connecting to the database", error)
+    //   })
+
+    // Iteration 3
+
     Recipe.insertMany(data)
+      .then(
+        data.forEach((element) => {
+          console.log(`${element.title}`)
+        })
+      )
+      //Iteration 4
+      .then(() => {
+        Recipe.findOneAndUpdate(
+          { name: "Rigatoni alla Genovese" }, // it doesnt work
+          { duration: 100 }
+        ).then(() => console.log("update successful"))
+      })
+
+      //Iteration 5
+      .then(() => {
+        Recipe.deleteOne({ title: "Carrot Cake" }) // it doesnt work
+        console.log("deleted")
+      })
+
+    setTimeout(() => {
+      mongoose.connection.close()
+      console.log("connection stop")
+    }, 400)
   })
